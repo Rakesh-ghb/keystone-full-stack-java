9d0f32fc25030365f8734731f22998f042fe8112
Rakesh Gorre <rakeshrakeshgorre@gmail.com>
2026-09-18 18:04:11 +0530

    Update SecurityConfig.java

--- src/main/java/com/key_stone/security/SecurityConfig.java
+++ src/main/java/com/key_stone/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.key_stone.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -20,6 +21,9 @@ import java.util.List;
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtFilter;
+
+    @Value("${FRONTEND_URL:http://localhost:5173}")
+    private String frontendUrl;
 
     public SecurityConfig(JwtAuthFilter jwtFilter) {
         this.jwtFilter = jwtFilter;
@@ -72,7 +76,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(
                 List.of(
                         "http://localhost:5173",
-                        "http://localhost:3000"
+                        "http://localhost:3000",
+                        frontendUrl
                 )
         );
 
