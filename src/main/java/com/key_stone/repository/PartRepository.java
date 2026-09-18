ae4eacef9adb282fc7c1d4618282caa82329d19f
Rakesh Gorre <rakeshrakeshgorre@gmail.com>
2026-09-18 08:30:12 +0530

    Update PartRepository.java

--- src/main/java/com/key_stone/repository/PartRepository.java
+++ src/main/java/com/key_stone/repository/PartRepository.java
@@ -1,4 +1,10 @@
 package com.key_stone.repository;
+
 import com.key_stone.domain.Part;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface PartRepository extends JpaRepository<Part,Long>{ }
+
+public interface PartRepository extends JpaRepository<Part, Long> {
+
+    boolean existsBySkuIgnoreCase(String sku);
+
+}
